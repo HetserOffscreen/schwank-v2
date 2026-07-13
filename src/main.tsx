c9d6105ab1009fac4f48f107b7fa592caba5c487
HetserOffscreen <102925464+HetserOffscreen@users.noreply.github.com>
2026-07-13 04:55:48 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,22 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { supabase } from './supabaseClient';
 import App from './App.tsx';
 import './index.css';
+
+const logPageView = async () => {
+    try {
+        await supabase.from('site_logs').insert([
+            { 
+                action: 'page_view',
+                selected_lang: navigator.language 
+            }
+        ]);
+    } catch (err) {
+        console.error("Failed to send log:", err);
+    }
+};
+logPageView();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
